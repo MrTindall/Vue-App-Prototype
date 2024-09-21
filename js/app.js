58da97787de59ced26e01eac68db22b27f3e1958
Alex <tindall.al@gmail.com>
2024-09-21 10:36:51 -0500

    added vue and starting vue functionality

---
+++ js/app.js
@@ -0,0 +1,56 @@
+const app = Vue.createApp({
+    // data: all the data for the app, must return an object
+    data: function() {
+        return {
+            exercise: {
+                name: '',
+                weight: 0,
+                sets: 0,
+                remove: false,
+            },
+            exerciseList: [
+                {exercise: 'Bench Press', weight: 185, sets: 3, remove: true},
+                {exercise: 'Incline DB Press', weight: 50, sets: 3, remove: false},
+                {exercise: 'Pushups', weight: 0, sets: 3, remove: false},
+            ],
+        }
+    },
+
+    // methods: usually "events" triggered by v-on:
+    methods: {
+        addSet(item) {
+            item.sets++
+        },
+
+        subtractSet(item) {
+            if(item.sets > 0) {
+                item.sets--;
+            }
+        },
+
+        addWeight(item) {
+            item.weight = item.weight + 2.5
+        },
+
+        subtractWeight(item) {
+            if(item.weight > 0) {
+                item.weight -= 2.5;
+            }
+        },
+    },
+
+    // computed: values that are updated and cached if dependencies change
+    computed: {
+
+    },
+
+    //mounted:  called after the instance has been mounted,
+    mounted: function () {
+
+    },
+
+    // watch:   calls the function if the value changes
+    watch: {
+
+    }
+});
