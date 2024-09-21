10689ecd4a5f7b67ab603cd92d30cf6b1516cc05
Alex <tindall.al@gmail.com>
2024-09-21 14:38:43 -0500

    fixed some js

--- js/app.js
+++ js/app.js
@@ -9,66 +9,64 @@ const app = Vue.createApp({
                 remove: false,
             },
             exerciseList: [
-                {exercise: 'Bench Press', weight: 185, sets: 3, remove: false},
-                {exercise: 'Incline DB Press', weight: 50, sets: 3, remove: false},
-                {exercise: 'Pushups', weight: 0, sets: 3, remove: false},
+                {name: 'Bench Press', weight: 185, sets: 3, remove: false},
+                {name: 'Incline DB Press', weight: 50, sets: 3, remove: false},
+                {name: 'Pushups', weight: 0, sets: 3, remove: false},
             ],
         }
     },
 
     // methods: usually "events" triggered by v-on:
     methods: {
-        addSet(item) {
-            item.sets++
+        addSet(exercise) {
+            exercise.sets++
         },
 
-        subtractSet(item) {
-            if(item.sets > 0) {
-                item.sets--;
+        subtractSet(exercise) {
+            if(exercise.sets > 0) {
+                exercise.sets--;
             }
         },
 
-        addWeight(item) {
-            item.weight = item.weight + 2.5
+        addWeight(exercise) {
+            exercise.weight = exercise.weight + 2.5
         },
 
-        subtractWeight(item) {
-            if(item.weight > 0) {
-                item.weight -= 2.5;
+        subtractWeight(exercise) {
+            if(exercise.weight > 0) {
+                exercise.weight -= 2.5;
             }
         },
 
         // TODO: need to get this working
-        removeGrtZeroExercise(removeGrtZero) {
-            for(item in removeGrtZero){
-                item.sets - item.sets;
-            }
+        removeGrtZeroExercise(itemList) {
+            
         }
     },
 
     // computed: values that are updated and cached if dependencies change
     computed: {
         todoList(){
-            return this.exerciseList.filter(function(item) {
-                return item.sets > 0;
+            return this.exerciseList.filter(function(exercise) {
+                return exercise.sets > 0;
             })
         },
 
         completeList(){
-            return this.exerciseList.filter(function(item) {
-                return item.sets <= 0;
+            return this.exerciseList.filter(function(exercise) {
+                return exercise.sets <= 0;
             })
         },
 
         removeGrtZero(){
-            return this.exerciseList.filter(function(item){
-                return item.remove == true && item.sets > 0
+            return this.exerciseList.filter(function(exercise){
+                return exercise.remove == true && exercise.sets > 0
             })
         },
 
         removeEquZero(){
-            return this.exerciseList.filter(function(item){
-                return item.remove == true && item.sets === 0
+            return this.exerciseList.filter(function(exercise){
+                return exercise.remove == true && exercise.sets === 0
             })
         },
 
