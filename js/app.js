28d08569db2acfbae90ec342dc412c600f6617d5
Alex <tindall.al@gmail.com>
2024-09-22 13:23:20 -0500

    added more functionality

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 const app = Vue.createApp({
     // data: all the data for the app, must return an object
-    data: function() {
+    data: function () {
         return {
             exercise: {
+                workoutName: '',
                 name: '',
                 weight: 0,
                 sets: 0,
@@ -11,9 +12,11 @@ const app = Vue.createApp({
 
             },
             exerciseList: [
-                {name: 'Bench Press', weight: 185, sets: 3, remove: false, completeRemove: false},
-                {name: 'Incline DB Press', weight: 50, sets: 3, remove: false, completeRemove: false},
-                {name: 'Pushups', weight: 0, sets: 3, remove: false, completeRemove: false},
+                { workoutName: 'Push', name: 'Bench Press', weight: 185, sets: 3, remove: false, completeRemove: false },
+                { workoutName: 'Push', name: 'Incline DB Press', weight: 50, sets: 3, remove: false, completeRemove: false },
+                { workoutName: 'Push', name: 'Pushups', weight: 0, sets: 3, remove: false, completeRemove: false },
+                { workoutName: 'Legs', name: 'Squat', weight: 235, sets: 3, remove: false, completeRemove: false },
+                { workoutName: 'Pull', name: 'Pullups', weight: 235, sets: 3, remove: false, completeRemove: false },
             ],
         }
     },
@@ -25,7 +28,7 @@ const app = Vue.createApp({
         },
 
         subtractSet(exercise) {
-            if(exercise.sets > 0) {
+            if (exercise.sets > 0) {
                 exercise.sets--;
             }
         },
@@ -35,51 +38,68 @@ const app = Vue.createApp({
         },
 
         subtractWeight(exercise) {
-            if(exercise.weight > 0) {
+            if (exercise.weight > 0) {
                 exercise.weight -= 2.5;
             }
         },
 
         removeGrtZeroExercise(list) {
-            for(exercise of list) {
+            for (exercise of list) {
                 exercise.sets = 0;
                 exercise.remove = false;
-            } 
+            }
         },
 
         removeEquZeroExercise(list) {
-            for(exercise of list) {
+            for (exercise of list) {
                 exercise.completeRemove = true;
-            } 
+            }
         },
+
+        addExercise() {
+            this.exerciseList.push(this.exercise);
+        }
     },
 
     // computed: values that are updated and cached if dependencies change
     computed: {
-        todoList(){
-            return this.exerciseList.filter(function(exercise) {
+        todoList() {
+            return this.exerciseList.filter(function (exercise) {
                 return exercise.sets > 0;
             })
         },
 
-        completeList(){
-            return this.exerciseList.filter(function(exercise) {
+        completeList() {
+            return this.exerciseList.filter(function (exercise) {
                 return exercise.sets <= 0 && exercise.completeRemove == false;
             })
         },
 
-        removeGrtZero(){
-            return this.exerciseList.filter(function(exercise){
-                return exercise.remove == true && exercise.sets > 0 
+        removeGrtZero() {
+            return this.exerciseList.filter(function (exercise) {
+                return exercise.remove == true && exercise.sets > 0
             })
         },
 
-        removeEquZero(){
-            return this.exerciseList.filter(function(exercise){
+        removeEquZero() {
+            return this.exerciseList.filter(function (exercise) {
                 return exercise.remove == true && exercise.completeRemove == false
             })
         },
 
+        workoutLibraryList() {
+            const uniqueWorkoutNames = new Set();
+
+            this.exerciseList.forEach(function (exercise) {
+                uniqueWorkoutNames.add(exercise.workoutName);
+            });
+
+            return Array.from(uniqueWorkoutNames);
+        },
+
+        exerciseLibraryList() {
+        
+        },
     },
 
     //mounted:  called after the instance has been mounted,
