a20f0e89465c1385d12e4e5b6bf546f1fd57aba9
Alex Tindall <tindall.al@gmail.com>
2024-11-11 14:48:26 -0600

    removed margin

--- js/components/exercise-list.js
+++ js/components/exercise-list.js
@@ -14,7 +14,7 @@ app.component('ExerciseList', {
     },
     template: `
         <div class="d-flex flex-wrap mb-3 justify-content-start mx-2">
-            <div v-for="item in list" :key="item.name" class="col-xxl-2 col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 mb-4">
+            <div v-for="item in list" :key="item.name" class="col-xxl-2 col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12">
                 <component 
                     :is="item.constructor.type + 'Exercise'"
                     :item="item" 
