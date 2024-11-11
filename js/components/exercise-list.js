78b500c0c78edb52d0e0c69a7028d2fbea3b3968
Alex Tindall <tindall.al@gmail.com>
2024-11-11 14:19:27 -0600

    fixed grid system to scale better

--- js/components/exercise-list.js
+++ js/components/exercise-list.js
@@ -14,14 +14,14 @@ app.component('ExerciseList', {
     },
     template: `
         <div class="d-flex flex-wrap mb-3 justify-content-start mx-2">
-            <component 
-                v-for="item in list"
-                :is="item.constructor.type + 'Exercise'"
-                :item="item" 
-                :isButton="isButton"
-                :key="item.name"
-                class="mb-1"
-            ></component>
+            <div v-for="item in list" :key="item.name" class="col-xxl-2 col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 mb-4">
+                <component 
+                    :is="item.constructor.type + 'Exercise'"
+                    :item="item" 
+                    :isButton="isButton"
+                    class="mb-1"
+                ></component>
+            </div>
         </div>
     `
 });
